000a905f21014f532dfc85d360e22d547bd58c15
Stefano <lupos@tcd.ie>
2017-10-13 17:11:56 +0100

    Clients can broadcast (and re-receive) using telnet

--- server.hs
+++ server.hs
@@ -9,6 +9,7 @@ import Control.Concurrent
 
 -- No idea yet
 import Control.Monad.Fix (fix)
+import Control.Monad (when)
 
 port :: PortNumber
 port = 3000
@@ -21,7 +22,7 @@ main = do
     listen sock 2                               -- set a max of 2 queued connections
     chan <- newChan                             -- Create Channel for thread communications
     print $ "Listening on port " ++ show port
-    acceptConnections sock chan
+    acceptConnections sock chan 0
 
 
 -- Semantics
@@ -29,40 +30,51 @@ type Msg = String
 
 -- Repeatedly waits for incoming socket connections
 -- Spawns a new thread to handle each connection it receives
-acceptConnections :: Socket -> Chan Msg -> IO ()
-acceptConnections sock chan = do
+acceptConnections :: Socket -> Chan Msg -> Int -> IO ()
+acceptConnections sock chan currentId = do
     conn <- accept sock         -- accept a connection and handle it
-    forkIO (handleClient conn chan)
-    acceptConnections sock chan
+    forkIO (handleClient conn chan currentId)
+    acceptConnections sock chan $ currentId+1
 
 -- Note dupChan makes a copy of the channel
 -- any data written to it OR written to the initial channel will be available at both channel
 -- This gives us broadcast functionality between threads
 
-handleClient :: (Socket, SockAddr) -> Chan Msg -> IO ()
-handleClient (sock, _) chan = do
-  let broadcast msg = writeChan chan msg        -- Define a function for broadcasting
+handleClient :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
+handleClient (sock, _) chan clientId = do
   handle <- socketToHandle sock ReadWriteMode
   hSetBuffering handle NoBuffering
   commLine <- dupChan chan
 
-  -- fork off a thread for reading from the duplicated channel
-  forkIO $ fix $ \loop -> do
-      line <- readChan commLine
-      putStrLn ("Read from Channel: " ++ line)
-      hPutStrLn handle line
-      loop
+  -- Spawn a thread for monitoring the channel and sending new messages to this client
+  forkIO (broadCastMessageToClient handle commLine clientId)
+
+  readFromSocket handle chan clientId
+
 
   -- read lines from the socket broadcast to channel
-  fix $ \loop -> do
-      line <- hGetLine handle
-      putStrLn line
-      broadcast line
-      loop
-
-
-  -- handleConnection handle
+--   fix $ \loop -> do
+--       line <- hGetLine handle
+--       putStrLn line
+--       broadcast line
+--       loop
   hClose handle
+
+
+-- Reads messages from socket and broadcasts it to the threads' channel
+readFromSocket :: Handle -> Chan Msg -> Int -> IO ()
+readFromSocket handle channel clientId = do
+  line <- hGetLine handle
+  writeChan channel (show clientId ++ ": " ++ line)
+  readFromSocket handle channel clientId
+
+-- Read from the shared channel and send it to the client
+broadCastMessageToClient :: Handle -> Chan Msg -> Int -> IO ()
+broadCastMessageToClient handle commLine clientId = do
+  message <- readChan commLine
+  hPutStrLn handle message
+
+  broadCastMessageToClient handle commLine clientId
 
 
 
